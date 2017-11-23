0e6b3aabc4e4918ad34fa91258133316abc0fdb0
kremnik <kremnikov@gmail.com>
2017-11-23 16:51:11 +0000

    Functions

--- emb.py
+++ emb.py
@@ -4,103 +4,159 @@ from __future__ import print_function
 
 import os.path
 import math
+import re
 import tensorflow as tf
 import numpy as np
 import facenet
-from sklearn.manifold import TSNE # for t-SNE dimension reduction
 from sklearn.decomposition import PCA # for PCA dimension reduction
 
-def main():
-	with tf.Graph().as_default():
-		with tf.Session() as sess:
-			data_set = facenet.get_dataset("../../datasets/ownpeople/ownpeople_mtcnnpy_160")
-			paths, label_list = facenet.get_image_paths_and_labels(data_set)
-			
-			# paths = paths[0:5] # just 5 pictures for speed
-			
-			print("Loading model")
-			facenet.load_model("20170511-185253")
-			images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
-			embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
-			phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
-			
-			image_size = 160
-			embedding_size = embeddings.get_shape()[1]
-			
-			print('Runnning forward pass')
-			batch_size = 10
-			nrof_images = len(paths)
-			nrof_batches = int(math.ceil(1.0*nrof_images / batch_size))
-			emb_array = np.zeros((nrof_images, embedding_size))
-			
-			for i in range(nrof_batches):
-				start_index = i*batch_size
-				end_index = min((i+1)*batch_size, nrof_images)
-				paths_batch = paths[start_index:end_index]
-				images = facenet.load_data(paths_batch, False, False, image_size)
-				feed_dict = { images_placeholder:images, phase_train_placeholder:False }
-				emb_array[start_index:end_index,:] = sess.run(embeddings, feed_dict=feed_dict)
-			
-			n_images = len(paths)
-			threshold = 1.0
-			print('Distance matrix')
-			print('    ', end='')
-			for i in range(len(paths)):
-				print('    %3d     ' % i, end='')
-			print('')
-			for i in range(len(paths)):
-				print('%1d  ' % i, end='')
-				for j in range(len(paths)):
-					dist = np.linalg.norm(emb_array[i,:] - emb_array[j,:])
-					print('  %1.4f' % dist, end='')
-					if (label_list[i] == label_list[j]) and (dist <= threshold):
-						print("(TP)", end='')
-					elif (label_list[i] != label_list[j]) and (dist > threshold):
-						print("(TN)", end='')
-					elif (label_list[i] == label_list[j]) and (dist > threshold):
-						print("(FN)", end='')
-					elif (label_list[i] != label_list[j]) and (dist <= threshold):
-						print("(FP)", end='')
-				print('')
+def get_model_filenames(model_dir):
+    files = os.listdir(model_dir)
+    meta_files = [s for s in files if s.endswith('.meta')]
+    if len(meta_files)==0:
+        raise ValueError('No meta file found in the model directory (%s)' % model_dir)
+    elif len(meta_files)>1:
+        raise ValueError('There should not be more than one meta file in the model directory (%s)' % model_dir)
+    meta_file = meta_files[0]
+    meta_files = [s for s in files if '.ckpt' in s]
+    max_step = -1
+    for f in files:
+        step_str = re.match(r'(^model-[\w\- ]+.ckpt-(\d+))', f)
+        if step_str is not None and len(step_str.groups())>=2:
+            step = int(step_str.groups()[1])
+            if step > max_step:
+                max_step = step
+                ckpt_file = step_str.groups()[0]
+    return meta_file, ckpt_file
 
-			# t-SNE dimension reduction
-			print('')
-			print("t-SNE dimension reduction")
-			emb_tsne = TSNE(n_components=2).fit_transform(emb_array)
-			print(emb_tsne)
-			
-			# PCA dimension reduction
-			print('')
-			print("PCA dimension reduction")
-			pca = PCA(n_components=7) # number of components must be either less or equal to number of training (or test) examples
-			emb_pca = pca.fit(emb_array).transform(emb_array)
-			print(emb_pca)
-			
-			# Fisher's discriminant
-			print('')
-			print('Fisher\'s discriminant')
-			E = np.linalg.inv(np.cov(emb_pca.T))
-			num = 5
-			y = emb_pca[num,:] # train example
-			
-			test_num = 0;
-			test = emb_pca[test_num,:] # test example
-			
-			emb_pca = np.delete(emb_pca, (test_num), axis=0) # cutting off the test example from the train set
-			emb_del = np.delete(emb_pca, (num), axis=0)
-			
-			D = (y - (1 / (len(emb_pca) - 1) * np.sum(emb_del)))
-			w = np.dot(E, D)
-			c = np.dot(w / np.linalg.norm(w), y)
-			
-			# Mahalanobis' distance
-			l = np.dot(w / np.linalg.norm(w), test) - c
-			
-			# label_del = np.delete(label_list, (test_num), axis=0) # cutting off the test label from the train labels set
-			# label_del = np.delete(label_list, (num), axis=0)
-			
-			print('')
-			print("l = ", l)
+def load_model(model, sess):
+    model_exp = os.path.expanduser(model)
+    print('Model directory: %s' % model_exp)
+    meta_file, ckpt_file = get_model_filenames(model_exp)
+
+    print('Metagraph file: %s' % meta_file)
+    print('Checkpoint file: %s' % ckpt_file)
+
+    saver = tf.train.import_meta_graph(os.path.join(model_exp, meta_file))
+    saver.restore(sess, os.path.join(model_exp, ckpt_file))
+
+def forward(paths, batch_size, images_placeholder, embeddings, phase_train_placeholder, image_size, embedding_size, sess):
+    batch_size = 10
+    nrof_images = len(paths)
+    nrof_batches = int(math.ceil(1.0*nrof_images / batch_size))
+    emb_array = np.zeros((nrof_images, embedding_size))
+
+    for i in range(nrof_batches):    
+        start_index = i*batch_size
+        end_index = min((i+1)*batch_size, nrof_images)
+        paths_batch = paths[start_index:end_index]
+        images = facenet.load_data(paths_batch, False, False, image_size)
+        feed_dict = { images_placeholder:images, phase_train_placeholder:False }
+        emb_array[start_index:end_index,:] = sess.run(embeddings, feed_dict=feed_dict)    
+        print("batch number:", i)
+    print("Complete")
+    
+    return emb_array
+
+def printDistance(emb_array, label_list, end=10):
+    threshold = 1.0
+    lng = len(emb_array[:end])
+    print('Distance matrix')
+    print('    ', end='')
+    for i in range(lng):
+        print('    %3d     ' % i, end='')
+    print('')
+    for i in range(lng):
+        print('%1d  ' % i, end='')
+        for j in range(lng):
+            dist = np.linalg.norm(emb_array[i,:] - emb_array[j,:])
+            print('  %1.4f' % dist, end='')
+            if (label_list[i] == label_list[j]) and (dist <= threshold):
+                print("(TP)", end='')
+            elif (label_list[i] != label_list[j]) and (dist > threshold):
+                print("(TN)", end='')
+            elif (label_list[i] == label_list[j]) and (dist > threshold):
+                print("\033[93m(FN)\033[0m", end='')
+            elif (label_list[i] != label_list[j]) and (dist <= threshold):
+                print("\033[93m(FP)\033[0m", end='')
+        print('')
+        
+def PCAtransform(emb_array, n_components = 10):
+    pca = PCA(n_components, whiten=True)
+    pca.fit(emb_array)
+    return pca.transform(emb_array)
+
+def splitTrainTest(data_set, percent_train):
+    paths, labels_list = facenet.get_image_paths_and_labels(data_set)
+    
+    lng = len(paths)
+    lng_train = int(lng * percent_train / 100);
+    lng_test = lng - lng_train
+    
+    train_paths = paths[0:lng_train]
+    test_paths = paths[lng_train:]
+    
+    train_labels = labels_list[0:lng_train]
+    test_labels = labels_list[lng_train:]
+    
+    return train_paths, train_labels, test_paths, test_labels
+	
+def trainFisher(emb_array, y):
+    emb_del = np.empty_like(emb_array)
+    np.copyto(emb_del, emb_array)
+    
+    emb_array = np.vstack((emb_array, y))
+    
+    # y = emb_array[sample_number]
+    # emb_del = np.delete(emb_array, (sample_number), axis=0)
+    
+    E = np.linalg.inv(np.cov(emb_array.T))
+    D = (y - (1 / (len(emb_array) - 1) * np.sum(emb_del)))
+    
+    w = np.dot(E, D)
+    c = np.dot(w / np.linalg.norm(w), y)
+    
+    return c, w
+
+def Fisher(w, c, sample):
+    return np.dot(w / np.linalg.norm(w), sample) - c
+	
+def main():
+	sess = tf.Session()
+
+	data_set = facenet.get_dataset("../../datasets/ownpeople/ownpeople_mtcnnpy_160")
+	train_paths, train_labels, test_paths, test_labels = splitTrainTest(data_set, 100)
+
+	# paths = paths[0:5] # just 5 pictures for speed
+
+	print("Loading model")
+	load_model("20170511-185253", sess)
+	images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+	embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+	phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+
+	image_size = 160
+	embedding_size = embeddings.get_shape()[1]
+
+	print('Runnning forward pass')
+	emb_array = forward(train_paths, 10, images_placeholder, embeddings, phase_train_placeholder, image_size, embedding_size, sess)
+
+	fisher_train = forward(["../../datasets/ownpeople/ownpeople_mtcnnpy_160/me/photo_2016-12-25_20-59-23.png"], 1, images_placeholder, embeddings, phase_train_placeholder, image_size, embedding_size, sess)
+	fisher_test_1 = forward(["../../datasets/ownpeople/ownpeople_mtcnnpy_160/me/photo_2016-12-25_20-59-23.png"], 1, images_placeholder, embeddings, phase_train_placeholder, image_size, embedding_size, sess)
+	fisher_test_2 = forward(["../../datasets/ownpeople/ownpeople_mtcnnpy_160/me/photo_2017-08-26_20-24-22.png"], 1, images_placeholder, embeddings, phase_train_placeholder, image_size, embedding_size, sess)
+	fisher_test_3 = forward(["../../datasets/ownpeople/ownpeople_mtcnnpy_160/me/photo_2017-10-31_16-36-11.png"], 1, images_placeholder, embeddings, phase_train_placeholder, image_size, embedding_size, sess)
+	fisher_test_4 = forward(["../../datasets/ownpeople/ownpeople_mtcnnpy_160/viks/photo_2017-05-19_00-35-56.png"], 1, images_placeholder, embeddings, phase_train_placeholder, image_size, embedding_size, sess)
+	fisher_test_5 = forward(["../../datasets/ownpeople/ownpeople_mtcnnpy_160/viks/photo_2017-07-15_17-05-07.png"], 1, images_placeholder, embeddings, phase_train_placeholder, image_size, embedding_size, sess)
+	fisher_test_6 = forward(["../../datasets/ownpeople/ownpeople_mtcnnpy_160/viks/photo_2017-09-26_00-32-31.png"], 1, images_placeholder, embeddings, phase_train_placeholder, image_size, embedding_size, sess)
+
+	# Training Fisher's discriminant
+	c, w = trainFisher(emb_array, fisher_train[0])
+
+	# Fisher's discriminant result
+	l = Fisher(w, c, fisher_test_6[0])
+	
+	print('')
+	print('Result:', l)
 	
 if __name__ == '__main__':
     main()
